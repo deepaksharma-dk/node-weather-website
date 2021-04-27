37892fab5a6a2a23b0ecf9f27bb19841f102f50a
Deepak Sharma <deepaksharma.dk@outlook.com>
2021-04-27 14:44:01 -0400

    removed consloe.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('Js loaded')
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#messageOne')
